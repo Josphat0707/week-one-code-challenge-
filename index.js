8f7be9893433d8aff169965c6efbb719e3acf84d
josphat <mjirura0707@gmail.com>
2023-06-17 18:19:40 +0300

    code done

---
+++ index.js
@@ -0,0 +1,23 @@
+function studentsGrades(){
+
+    let marks = prompt("Enter your marks!");
+
+    if(marks < 0 || marks > 100){
+        return "Invalid marks";
+    }
+    
+    let grade;
+    if(marks >= 79){
+        grade = "A";
+    }else if(marks >= 60 && marks < 79){
+        grade = "B";
+    }else if(marks >= 49 && marks <= 59){
+        grade = "C";
+    }else if(marks >= 40 && marks <49){
+        grade = "D";
+    }else{
+        grade = "E";
+    }
+    
+    return grade;
+}
