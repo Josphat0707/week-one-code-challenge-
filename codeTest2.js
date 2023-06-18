fc1d860c60b9faa82817ea1a10f2e5f65d33f5e0
josphat <mjirura0707@gmail.com>
2023-06-18 11:45:28 +0300

    completed challenge 2

--- codeTest2.js
+++ codeTest2.js
@@ -1,16 +1,16 @@
 function speedDetector(speed){
-    const speedLimit = 70;
-    const demeritPoint = 5;
-    const maxPointsGiven = 12;
+    const speedLimit = 70;//since the speed limit is a solid 70 i had to use the const keyword
+    const demeritPoint = 5;//a demerit point is awarded to a driver after going 5kilometers per hour past the speed limit
+    const maxPointsGiven = 12;//Maximum number of demerit points awarded is 12. Past that, you drivers license is cancelled
     if(speed <= speedLimit){
-        return "okay"
+        return "okay"// If one doesnt cross the speed limit, it returns okay
     }else {
         let excessSpeed = speed - speedLimit;
         const demeritPoints = Math.floor(speed/demeritPoint);
-        if(demeritPoints > maxPointsGiven){
+        if(demeritPoints > maxPointsGiven){//condition checks if one has crossed the maximum number of points given
             return "Cancel Licence!"
-        }else{"points" + demeritPoints};
+        }else{"points" + demeritPoints};//if one hasnt then it adds to the demeritpoints one already has
     }
 }
 const speed = 70//Enter your car speed
-console.log(speedDetector(speed));
+console.log(speedDetector(speed));//calls the function so as to run the code
